b2f64226e0450a3830523ec3bef0262d3427eab7
harshdipsinhJ <168002710+harshdipsinhJ@users.noreply.github.com>
2024-07-17 21:55:48 +0530

    Create typedef.c

---
+++ typedef.c
@@ -0,0 +1,14 @@
+//use of typedef
+#include<stdio.h>
+#include<string.h>
+
+typedef struct datafromharshdipsinh
+{
+    int roll;
+    int num;
+}cse;
+
+int main(){
+    cse student1={44,55};
+    printf("hoo %d jo%d",student1);
+}
